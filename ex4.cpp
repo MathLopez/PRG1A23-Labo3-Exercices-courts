1b4bb5e2ee55f5dfb6be5aeb345118b9c7f10181
Matheo Lopez <145534968+MathLopez@users.noreply.github.com>
2023-11-03 11:07:55 +0100

    Update ex4.cpp

--- ex4.cpp
+++ ex4.cpp
@@ -16,8 +16,35 @@ using namespace std;
 //Essayez encore : 12
 //Merci, vous avez entre 12
 
+int lire_multiple_de(int nb);
+bool is_number(const string& s);
+
 int main() {
 
    int n = lire_multiple_de(3);
    cout << "Merci, vous avez entre " << n << endl;
+}
+
+int lire_multiple_de(int nb){
+    string in;
+    cout << "Entrez un entier multiple de " << nb << ": ";
+    startFunc:
+    while ((cin >> in) and !is_number(in)){
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        cout << "Essayez encore : ";
+    }
+    if(stoi(in) % nb != 0){
+        cout << "Essayez encore : ";
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+        goto startFunc;
+    }
+    return stoi(in);
+}
+
+// https://stackoverflow.com/questions/4654636/how-to-determine-if-a-string-is-a-number-with-c
+bool is_number(const string& s)
+{
+    string::const_iterator it = s.begin();
+    while (it != s.end() && isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
 }
